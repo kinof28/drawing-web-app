760198a34eeb336da74248bc2d509b630d757c36
@kinof28 <ab28fb@gmail.com>
2021-08-14 05:45:16 +0100

    html and css

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function refresh(){
+    console.log("refresh")
+}
+function changeColor(e){
+    console.log(e)
+}
+function changeColorPicker(e){
+    console.log(e)
+}
+function changePinSize(e){
+        console.log(e)
+}
