e513a8c3181b3a74b33257925ae2f8f2120a75aa
@kinof28 <ab28fb@gmail.com>
2021-08-14 23:57:31 +0100

    some changes on draw lines style

--- js/script.js
+++ js/script.js
@@ -24,21 +24,23 @@ canvas.addEventListener("mousemove" ,(e)=>{
     }
 })
 const draw =(x1,y1)=>{
-    // console.log(x1,y1);
-    context.beginPath();
     //draw process
-    // context.fillStyle='blue';
-    // context.lineWidth = 5;
-    // context.moveTo(x, y);
-    // context.arc(x,y, x1,y1,0);
-    // context.fill();
-    context.beginPath();
     context.strokeStyle = color;
-    context.lineWidth = pinSize;
+    context.fillStyle = color;
+    context.lineWidth = pinSize*2;
+    context.beginPath();
+    // context.moveTo(x, y);
+    // context.arcTo(x1,y1, x,y, 2 * Math.PI);
+    // context.stroke();
+    context.arc(x1,y1,pinSize,0,Math.PI*2);
+    context.closePath();
+    context.fill();
+    context.beginPath();
     context.moveTo(x, y);
-    context.arcTo(x1,y1, x,y, 2 * Math.PI);
+    context.lineTo(x1,y1);//, x,y, 2 * Math.PI);
     context.stroke();
     context.closePath();
+    
     x=x1;
     y=y1;
 }
