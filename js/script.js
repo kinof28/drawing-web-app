1567a0a1e09ccbe0748249b9bed5a2bfae4f043a
@kinof28 <ab28fb@gmail.com>
2021-08-14 14:49:25 +0100

    fonctionality done need download and refresh and some modification in drawing style

--- js/script.js
+++ js/script.js
@@ -1,14 +1,71 @@
+const canvas=document.getElementById("canv1");
+const context =canvas.getContext('2d');
+let isDrawing=false;
+let picked=false;
+let color='hotpink';
+let pinSize=5;
+let x=undefined;
+let y=undefined;
+canvas.addEventListener('mousedown',(e)=>{
+    isDrawing=true;
+    x=e.offsetX;
+    y=e.offsetY;
+})
+canvas.addEventListener('mouseup' ,(e)=>{
+    isDrawing=false;
+    x=undefined;
+    y=undefined;
+})
+canvas.addEventListener("mousemove" ,(e)=>{
+    if(isDrawing){
+        draw(e.offsetX,e.offsetY);
+        // console.log(e.clientX);
+        // console.log(e.clientY);
+    }
+})
+const draw =(x1,y1)=>{
+    // console.log(x1,y1);
+    context.beginPath();
+    //draw process
+    // context.fillStyle='blue';
+    // context.lineWidth = 5;
+    // context.moveTo(x, y);
+    // context.arc(x,y, x1,y1,0);
+    // context.fill();
+    context.beginPath();
+    context.strokeStyle = color;
+    context.lineWidth = pinSize;
+    context.moveTo(x, y);
+    context.arcTo(x1,y1, x,y, 2 * Math.PI);
+    context.stroke();
+    context.closePath();
+    x=x1;
+    y=y1;
+}
 function refresh(){
     console.log("refresh")
 }
 function changeColor(e){
-    console.log(e)
+    let act;
+    if(!picked){
+        act=document.querySelector('[data-color="'+color+'"]');
+        act.classList.remove("active-color");
+    }
+    color=e.getAttribute("data-color");
+    act=document.querySelector('[data-color="'+color+'"]');
+    act.classList.add("active-color");
+    picked=false;
+
+
 }
 function changeColorPicker(e){
-    console.log(e)
+    let act=document.querySelector('[data-color="'+color+'"]');
+    act.classList.remove("active-color");
+    color=e;
+    picked=true;
 }
 function changePinSize(e){
-    console.log(e)
+    pinSize=e;
 }
 function downloadImg(e){
     console.log(e)
